773d196376e97c9957d93588311044935141f65e
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2024-11-26 18:07:16 +0100

    update script

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-
-
-
 <div align="center">
   
 ## About me
@@ -37,19 +34,12 @@
     [![linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/fabian-a-alvarez/)
    [![twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/_SantaCRC_)
 
-  
-  
-
-
 ## Stats
 [![GitHub Streak](http://github-readme-streak-stats.herokuapp.com?user=SantaCRC&theme=buefy-dark&hide_border=true&date_format=M%20j%5B%2C%20Y%5D&background=DD272700)](https://github.com/SantaCRC)
   
   [![trophy](https://github-profile-trophy.vercel.app/?username=santacrc&theme=discord&no-bg=true&no-frame=true&rank=SECRET,SSS,SS,S,AAA,AA,A,B,C&column=3)](https://github.com/SantaCRC)
 
+## Latest Instagram Posts
 
-
-  ## Latest Instagram Posts
-
-| ![](https://scontent-sjc3-1.cdninstagram.com/v/t51.75761-15/467941909_17853680571333196_7721653764865265119_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=18de74&_nc_ohc=4tkCSAUlvysQ7kNvgGm8mEs&_nc_zt=23&_nc_ht=scontent-sjc3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=A0isocNznJ-5lBTgGpxxBrk&oh=00_AYCcf8_mKNmEalIpEx4hw4EW8frY3tI62ppwqeuRR2OsXw&oe=6749C599) | ![](https://scontent-sjc3-1.cdninstagram.com/v/t51.29350-15/468126752_459202540135949_6855116969796609433_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=18de74&_nc_ohc=o03ZD_txfVQQ7kNvgEAF9WK&_nc_zt=23&_nc_ht=scontent-sjc3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=A0isocNznJ-5lBTgGpxxBrk&oh=00_AYDL518Ku-kn_J9CjxgS2U6QZ1Ekq01NAPJzXS2gfLIyaA&oe=6749A345) | ![](https://scontent-sjc3-1.cdninstagram.com/v/t51.75761-15/467649455_17853337398333196_1670008221207277219_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=KEIX61qeUXMQ7kNvgGJOTL8&_nc_zt=23&_nc_ht=scontent-sjc3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=A0isocNznJ-5lBTgGpxxBrk&oh=00_AYBa9SR9gFmIeDoKxnJt7nrS9YdWi9-oN6pgrqzd5ASHVQ&oe=6749AFEF) |
+ [![](https://scontent-sjc3-1.cdninstagram.com/v/t51.75761-15/467941909_17853680571333196_7721653764865265119_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=18de74&_nc_ohc=4tkCSAUlvysQ7kNvgGm8mEs&_nc_zt=23&_nc_ht=scontent-sjc3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=A0isocNznJ-5lBTgGpxxBrk&oh=00_AYCcf8_mKNmEalIpEx4hw4EW8frY3tI62ppwqeuRR2OsXw&oe=6749C599)](https://www.instagram.com/reel/DCtNdK9oQO7/) | [![](https://scontent-sjc3-1.cdninstagram.com/v/t51.29350-15/468126752_459202540135949_6855116969796609433_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=18de74&_nc_ohc=o03ZD_txfVQQ7kNvgEAF9WK&_nc_zt=23&_nc_ht=scontent-sjc3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=A0isocNznJ-5lBTgGpxxBrk&oh=00_AYDL518Ku-kn_J9CjxgS2U6QZ1Ekq01NAPJzXS2gfLIyaA&oe=6749A345)](https://www.instagram.com/p/DCrD918tKqd/) | [![](https://scontent-sjc3-1.cdninstagram.com/v/t51.75761-15/467649455_17853337398333196_1670008221207277219_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=18de74&_nc_ohc=KEIX61qeUXMQ7kNvgGJOTL8&_nc_zt=23&_nc_ht=scontent-sjc3-1.cdninstagram.com&edm=ANo9K5cEAAAA&_nc_gid=A0isocNznJ-5lBTgGpxxBrk&oh=00_AYBa9SR9gFmIeDoKxnJt7nrS9YdWi9-oN6pgrqzd5ASHVQ&oe=6749AFEF)](https://www.instagram.com/reel/DCmznP7NnIY/) |
 |---|---|---|
-<div>
--- scripts/update.py
+++ scripts/update.py
@@ -1,7 +1,49 @@
 import requests
-import re
 
-# URL del archivo JSON de Instagram
+# Parte constante del README
+README_CONSTANT = """<div align="center">
+  
+## About me
+  
+[![Typing SVG](https://readme-typing-svg.herokuapp.com?color=%2336BCF7&center=true&multiline=true&width=420&height=100&lines=Hi!+my+name+is+Fabian;I+am+mechatronics+engineering+student;from+Costa+Rica)](https://github.com/SantaCRC)
+
+### Programming languages
+  ![C](https://img.shields.io/badge/c-%2300599C.svg?style=for-the-badge&logo=c&logoColor=white)
+  ![C#](https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white)
+![Dart](https://img.shields.io/badge/dart-%230175C2.svg?style=for-the-badge&logo=dart&logoColor=white)
+![C++](https://img.shields.io/badge/c++-%2300599C.svg?style=for-the-badge&logo=c%2B%2B&logoColor=white)
+![Java](https://img.shields.io/badge/java-%23ED8B00.svg?style=for-the-badge&logo=java&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![Octave](https://img.shields.io/badge/OCTAVE-darkblue?style=for-the-badge&logo=octave&logoColor=fcd683)
+  ![PHP](https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white)
+  ![Python](https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54)
+
+  ### Frameworks
+  ![Anaconda](https://img.shields.io/badge/Anaconda-%2344A833.svg?style=for-the-badge&logo=anaconda&logoColor=white)
+  ![Angular](https://img.shields.io/badge/angular-%23DD0031.svg?style=for-the-badge&logo=angular&logoColor=white)
+  ![Angular.js](https://img.shields.io/badge/angular.js-%23E23237.svg?style=for-the-badge&logo=angularjs&logoColor=white)
+  ![Chart.js](https://img.shields.io/badge/chart.js-F5788D.svg?style=for-the-badge&logo=chart.js&logoColor=white)
+  ![Django](https://img.shields.io/badge/django-%23092E20.svg?style=for-the-badge&logo=django&logoColor=white)
+  ![Expo](https://img.shields.io/badge/expo-1C1E24?style=for-the-badge&logo=expo&logoColor=#D04A37)
+  ![Flask](https://img.shields.io/badge/flask-%23000.svg?style=for-the-badge&logo=flask&logoColor=white)
+![Flutter](https://img.shields.io/badge/Flutter-%2302569B.svg?style=for-the-badge&logo=Flutter&logoColor=white)
+  ![OpenCV](https://img.shields.io/badge/opencv-%23white.svg?style=for-the-badge&logo=opencv&logoColor=white)
+![Threejs](https://img.shields.io/badge/threejs-black?style=for-the-badge&logo=three.js&logoColor=white)
+![Arduino](https://img.shields.io/badge/-Arduino-00979D?style=for-the-badge&logo=Arduino&logoColor=white)
+  ![Tensor](https://img.shields.io/badge/TensorFlow-FF6F00?style=for-the-badge&logo=TensorFlow&logoColor=white)
+  
+  ### Social
+  [![Website](https://img.shields.io/badge/website-000000?style=for-the-badge&logo=About.me&logoColor=white)](https://fabianalvarez.dev)
+    [![linkedin](https://img.shields.io/badge/LinkedIn-0077B5?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/fabian-a-alvarez/)
+   [![twitter](https://img.shields.io/badge/Twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/_SantaCRC_)
+
+## Stats
+[![GitHub Streak](http://github-readme-streak-stats.herokuapp.com?user=SantaCRC&theme=buefy-dark&hide_border=true&date_format=M%20j%5B%2C%20Y%5D&background=DD272700)](https://github.com/SantaCRC)
+  
+  [![trophy](https://github-profile-trophy.vercel.app/?username=santacrc&theme=discord&no-bg=true&no-frame=true&rank=SECRET,SSS,SS,S,AAA,AA,A,B,C&column=3)](https://github.com/SantaCRC)
+"""
+
+# Obtener las últimas publicaciones de Instagram
 url = 'https://fabianalvarez.dev/instagram.json'
 
 # Obtener los datos del JSON
@@ -16,34 +58,22 @@ else:
 latest_posts = data[:3]
 
 # Generar la tabla Markdown con enlaces a Instagram
-markdown_table = '## Latest Instagram Posts\n\n'
-markdown_table += '|---|---|---|\n'
+instagram_section = '## Latest Instagram Posts\n\n'
+
 
 # Recolectar las URLs de las imágenes y enlaces de las publicaciones
-image_links = [
-    (post.get('image', ''), post.get('link', '#')) for post in latest_posts
-]
+for post in latest_posts:
+    image_url = post.get('image', '')
+    post_link = post.get('link', '#')
+    instagram_section += f' [![]({image_url})]({post_link}) |'
 
-# Añadir las imágenes con enlaces a la tabla
-markdown_table += '|'
-for image_url, post_link in image_links:
-    markdown_table += f' [![]({image_url})]({post_link}) |'
-markdown_table += '\n'
+instagram_section += '\n'
+instagram_section += '|---|---|---|\n'
 
-# Leer el archivo README.md
-with open('README.md', 'r', encoding='utf-8') as file:
-    readme_content = file.read()
-
-# Reemplazar toda la sección existente "## Latest Instagram Posts"
-updated_content = re.sub(
-    r'## Latest Instagram Posts\n\n.*?\n\n',
-    markdown_table + '\n',
-    readme_content,
-    flags=re.DOTALL
-)
-
-# Escribir el contenido actualizado en README.md
+# Crear el archivo README.md
 with open('README.md', 'w', encoding='utf-8') as file:
-    file.write(updated_content)
+    file.write(README_CONSTANT)
+    file.write('\n')
+    file.write(instagram_section)
 
-print('La sección "## Latest Instagram Posts" ha sido actualizada con éxito.')
+print("README.md generado con éxito.")
